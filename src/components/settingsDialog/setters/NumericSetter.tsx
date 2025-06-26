163f483f03c801974b209cd1fd80de94fa64dc2e
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2025-06-25 17:34:24 -0700

    Add validation.

--- src/components/settingsDialog/setters/NumericSetter.tsx
+++ src/components/settingsDialog/setters/NumericSetter.tsx
@@ -1,24 +1,43 @@
+import { useState } from "react";
+
 import NumericRangeSetting from "@/settings/types/NumericSetting";
 import styles from "./Setters.module.css";
 import NumericInput from "@/components/numericInput/NumericInput";
+import { ValidateSettingCallback } from "@/components/settingsDialog/types/AppSettingsCallbacks";
+import { LAST_VALID_VALUE } from "@/components/settingsDialog/types/ValidationFailure";
 
 type Props = {
   setting:NumericRangeSetting,
-  onChange:(setting:NumericRangeSetting, isValid:boolean) => void,  
+  onChange:(setting:NumericRangeSetting, isValid:boolean) => void,
+  onValidateSetting?:ValidateSettingCallback
 }
 
-function NumericSetter({ setting, onChange }:Props) {
+function NumericSetter({ setting, onChange, onValidateSetting }:Props) {
+  const [validationMessage, setValidationMessage] = useState<string | null>(null);
   
   function _onChange(value:number) {
     const nextSetting = { ...setting, value };
-    // Don't need validation, because the NumericInputRange component doesn't allow invalid values.
-    onChange(nextSetting, true);
+    let isValid = true;
+    if (onValidateSetting) {
+      const validationFailure = onValidateSetting(nextSetting);
+      if (validationFailure) {
+        isValid = false;
+        setValidationMessage(validationFailure.failReason);
+        if (validationFailure.nextValue === LAST_VALID_VALUE) return;
+        if (validationFailure.nextValue !== undefined) nextSetting.value = validationFailure.nextValue;
+      } else {
+        setValidationMessage(null);
+      }
+    }
+    onChange(nextSetting, isValid);
   }
+  const validationContent = validationMessage ? <div className={styles.validationContent}>{validationMessage}</div> : null;
 
   return (
     <div className={styles.container}>
       <span className={styles.label}>{setting.label}</span>
-      <NumericInput minValue={setting.min} maxValue={setting.max} allowDecimals={setting.useDecimal} value={setting.value} onChange={_onChange} />
+      <NumericInput minValue={setting.minValue} maxValue={setting.maxValue} allowDecimals={setting.allowDecimals} value={setting.value} onChange={_onChange} />
+      {validationContent}
     </div>
   );
 }
