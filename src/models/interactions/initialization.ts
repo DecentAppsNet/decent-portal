7e44697ad480da02d5d52f5387594edfff1f5cf2
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2025-08-02 08:15:24 -0700

    Remove console logging.

--- src/models/interactions/initialization.ts
+++ src/models/interactions/initialization.ts
@@ -2,12 +2,9 @@ import { getAppMetaData } from "@/appMetadata/appMetadataUtil";
 import SupportedModel from "@/appMetadata/types/SupportedModel";
 
 export async function findOtherModelCount(supportedModels?:SupportedModel[]):Promise<number> {
-  console.log('!!1');
   if (!supportedModels) {
-    console.log('!!2');
     const appMetaData = await getAppMetaData();
     supportedModels = appMetaData.supportedModels;
   }
-  console.log('!!3');
   return Math.max(0, supportedModels.length - 1);
 }
